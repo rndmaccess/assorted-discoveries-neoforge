84d598f67c92bbf503d61505f19020cf1849c630
rndm_access <rndm.access.dev@gmail.com>
2026-06-03 16:28:26 -0600

    Fixed the wall stripping mechanics and added the missing wall variants.

--- src/main/java/rndm_access/assorteddiscoveries/mixin/AxeItemMixin.java
+++ src/main/java/rndm_access/assorteddiscoveries/mixin/AxeItemMixin.java
@@ -1,14 +1,16 @@
 package rndm_access.assorteddiscoveries.mixin;
 
-import com.google.common.collect.ImmutableMap;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.item.context.UseOnContext;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import oshi.util.tuples.Pair;
 import rndm_access.assorteddiscoveries.core.ModBlocks;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.minecraft.core.BlockPos;
@@ -25,18 +27,21 @@ import net.minecraft.world.level.block.state.BlockState;
 @Mixin(AxeItem.class)
 public abstract class AxeItemMixin {
     @Unique
-    private static final Map<DeferredBlock<Block>, DeferredBlock<Block>> STRIPPABLE_WALLS
-            = new ImmutableMap.Builder<DeferredBlock<Block>, DeferredBlock<Block>>()
-            .put(ModBlocks.OAK_WALL, ModBlocks.STRIPPED_OAK_WALL)
-            .put(ModBlocks.SPRUCE_WALL, ModBlocks.STRIPPED_SPRUCE_WALL)
-            .put(ModBlocks.BIRCH_WALL, ModBlocks.STRIPPED_BIRCH_WALL)
-            .put(ModBlocks.JUNGLE_WALL, ModBlocks.STRIPPED_JUNGLE_WALL)
-            .put(ModBlocks.ACACIA_WALL, ModBlocks.STRIPPED_ACACIA_WALL)
-            .put(ModBlocks.DARK_OAK_WALL, ModBlocks.STRIPPED_DARK_OAK_WALL)
-            .put(ModBlocks.MANGROVE_WALL, ModBlocks.STRIPPED_MANGROVE_WALL)
-            .put(ModBlocks.CRIMSON_WALL, ModBlocks.STRIPPED_CRIMSON_WALL)
-            .put(ModBlocks.WARPED_WALL, ModBlocks.STRIPPED_WARPED_WALL)
-            .put(ModBlocks.CHERRY_WALL, ModBlocks.STRIPPED_CHERRY_WALL).build();
+    private static final List<Pair<DeferredBlock<Block>, DeferredBlock<Block>>> WRAPPED_STRIPPABLE_WALLS
+            = List.of(new Pair<>(ModBlocks.OAK_WALL, ModBlocks.STRIPPED_OAK_WALL),
+                    new Pair<>(ModBlocks.SPRUCE_WALL, ModBlocks.STRIPPED_SPRUCE_WALL),
+                    new Pair<>(ModBlocks.BIRCH_WALL, ModBlocks.STRIPPED_BIRCH_WALL),
+                    new Pair<>(ModBlocks.JUNGLE_WALL, ModBlocks.STRIPPED_JUNGLE_WALL),
+                    new Pair<>(ModBlocks.ACACIA_WALL, ModBlocks.STRIPPED_ACACIA_WALL),
+                    new Pair<>(ModBlocks.DARK_OAK_WALL, ModBlocks.STRIPPED_DARK_OAK_WALL),
+                    new Pair<>(ModBlocks.MANGROVE_WALL, ModBlocks.STRIPPED_MANGROVE_WALL),
+                    new Pair<>(ModBlocks.CRIMSON_WALL, ModBlocks.STRIPPED_CRIMSON_WALL),
+                    new Pair<>(ModBlocks.WARPED_WALL, ModBlocks.STRIPPED_WARPED_WALL),
+                    new Pair<>(ModBlocks.CHERRY_WALL, ModBlocks.STRIPPED_CHERRY_WALL),
+                    new Pair<>(ModBlocks.PALE_OAK_WALL, ModBlocks.STRIPPED_PALE_OAK_WALL),
+                    new Pair<>(ModBlocks.BAMBOO_WALL, ModBlocks.STRIPPED_BAMBOO_WALL));
+    @Unique
+    private static final Map<Block, Block> STRIPPABLE_WALLS = new HashMap<>();
 
     @ModifyReturnValue(method = "useOn", at = @At("RETURN"))
     private InteractionResult useOn(InteractionResult original, UseOnContext context) {
@@ -46,7 +51,8 @@ public abstract class AxeItemMixin {
         BlockState state = world.getBlockState(pos);
         Block block = state.getBlock();
 
-        /*
+        assorted_discoveries_neoforge$unwrapStrippedWalls();
+
         if (STRIPPABLE_WALLS.containsKey(block) && block instanceof WallBlock) {
             world.playSound(player, pos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
             world.setBlockAndUpdate(pos, STRIPPABLE_WALLS.get(block).defaultBlockState()
@@ -58,7 +64,17 @@ public abstract class AxeItemMixin {
                     .setValue(WallBlock.WATERLOGGED, state.getValue(WallBlock.WATERLOGGED)));
             return InteractionResult.SUCCESS;
         }
-        */
         return original;
+    }
+
+    @Unique
+    private static void assorted_discoveries_neoforge$unwrapStrippedWalls() {
+        if (STRIPPABLE_WALLS.isEmpty()) {
+            for (Pair<DeferredBlock<Block>, DeferredBlock<Block>> entry : WRAPPED_STRIPPABLE_WALLS) {
+                Block wall = entry.getA().get();
+                Block strippedWall = entry.getB().get();
+                STRIPPABLE_WALLS.put(wall, strippedWall);
+            }
+        }
     }
 }
